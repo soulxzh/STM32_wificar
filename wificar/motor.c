415a32ef2f75ea2af5f61847c69a819e6598e5ba
soulxzh <tonychina40@gmail.com>
2019-05-04 23:43:22 +0800

    Left/Right move fixed.

--- wificar/motor.c
+++ wificar/motor.c
@@ -9,6 +9,26 @@
 //RIO LIO低电平         电机逆时针转动                    整体右转
 
 
+void MotorInit()
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+ 	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	 //使能PA端口时钟
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;				
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		
+	GPIO_Init(GPIOA, &GPIO_InitStructure);					 
+	GPIO_ResetBits(GPIOA,GPIO_Pin_2);						
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	  				 
+	GPIO_SetBits(GPIOA,GPIO_Pin_3); 		
+	TIM_SetCompare1(TIM3,3599);	
+	TIM_SetCompare2(TIM3,3599);
+}
 
 void CarGo()
 {
@@ -23,14 +43,14 @@ void CarGo()
 void CarLeft()
 {
 	TIM_SetCompare1(TIM3,49);
-	TIM_SetCompare2(TIM3,1799);
+	TIM_SetCompare2(TIM3,899);
 	RIO=0;
 	LIO=0;
 }
 
 void CarRight()
 {
-	TIM_SetCompare1(TIM3,1799);
+	TIM_SetCompare1(TIM3,899);
 	TIM_SetCompare2(TIM3,49);
 	RIO=1;	
 	LIO=1;
@@ -54,23 +74,3 @@ void CarStop()//需要U型BACK
 	delay_ms(500);
 }
 
-void MotorInit()
-{
-	GPIO_InitTypeDef  GPIO_InitStructure;
- 	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	 //使能PA端口时钟
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;				
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		
-	GPIO_Init(GPIOA, &GPIO_InitStructure);					 
-	GPIO_ResetBits(GPIOA,GPIO_Pin_2);						
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		
-	GPIO_Init(GPIOA, &GPIO_InitStructure);	  				 
-	GPIO_SetBits(GPIOA,GPIO_Pin_3); 		
-	TIM_SetCompare1(TIM3,3599);	
-	TIM_SetCompare2(TIM3,3599);
-}
